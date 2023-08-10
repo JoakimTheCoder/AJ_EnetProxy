4324b0fbcd7e0c1fed5c30a709a8aa602ae74972
JoakimTheCoder <113786004+JoakimTheCoder@users.noreply.github.com>
2023-08-10 14:59:36 +0300

    Add files via upload

---
+++ AJ_ProxyBase/README.md
@@ -0,0 +1,27 @@
+# Growtopia enet proxy (added more features)
+## OpenSSL Download https://slproweb.com/download/Win64OpenSSL-3_0_5.exe
+
+## Features
+* /proxy (Shows proxy commands)
+* /dropwl [amount]
+* /dropdl [amount]
+* /dropbgl [amount]
+* /wrenchset [pull/kick/ban] (Change wrench mode)
+* /wm (Enable/Disable Wrench Mode)
+* /tp [name] (Teleport To Target Player)
+* /ghost (Ghost Mode)
+* /uid [name] (Resolve Target UID)
+* /warp [world] (Warp To Other World)
+* /flag [id] (Change Flag To Item ID)
+* /skin [id] (Change Skin To Skin ID)
+* /country [key] (Change Flag To Other Country)
+* /name [name] (Change GrowID But Its Visual)
+* /fd (Enable/Disable Fast Drop)
+* /ft (Enable/Disable Fast Trash)
+* /pullall (Pull All Player In Same World)
+* /banall (Ban All Player In Same World)
+* /tradeall (Trade All Player In Same World)
+
+# By Joakim(https://github.com/JoakimTheCoder)
+
+
---
+++ AJ_ProxyBase/proxy/events.cpp
@@ -0,0 +1,316 @@
+#include "events.h"
+#include "gt.hpp"
+#include "proton/hash.hpp"
+#include "proton/rtparam.hpp"
+#include "proton/variant.hpp"
+#include "server.h"
+#include "utils.h"
+#include <thread>
+#include <limits.h>
+#include "HTTPRequest.hpp"
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+using namespace events::out;
+using namespace std;
+bool events::out::variantlist(gameupdatepacket_t* packet) {
+    variantlist_t varlist{};
+    varlist.serialize_from_mem(utils::get_extended(packet));
+    PRINTS("varlist: %s\n", varlist.print().c_str());
+    return false;
+}
+std::vector<std::string> split(const std::string& str, const std::string& delim)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos) pos = str.length();
+        std::string token = str.substr(prev, pos - prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    } while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+bool events::out::pingreply(gameupdatepacket_t* packet) {
+    //since this is a pointer we do not need to copy memory manually again
+    packet->m_vec2_x = 1000.f;  //gravity
+    packet->m_vec2_y = 250.f;   //move speed
+    packet->m_vec_x = 64.f;     //punch range
+    packet->m_vec_y = 64.f;     //build range
+    packet->m_jump_amount = 0;  //for example unlim jumps set it to high which causes ban
+    packet->m_player_flags = 0; //effect flags. good to have as 0 if using mod noclip, or etc.
+    return false;
+}
+bool fastdrop = false;
+bool fasttrash = false;
+std::string mode = "pull";
+bool events::out::generictext(std::string packet) {
+    if (!packet.find("game_version")) // hide login packet from console (it shows your password also)
+    PRINTS("Generic text: %s\n", packet.c_str());
+    auto& world = g_server->m_world;
+    rtvar var = rtvar::parse(packet);
+    if (!var.valid())
+        return false;
+    if (var.get(0).m_key == "action" && var.get(0).m_value == "input") {
+        if (var.size() < 2)
+            return false;
+        if (var.get(1).m_values.size() < 2)
+            return false;
+
+        if (!world.connected)
+            return false;
+
+        auto chat = var.get(1).m_values[1];
+
+        // Your Commands starts here.
+
+        if (chat == "/proxy") {
+            std::string dialog;
+            dialog =
+                "\nadd_label_with_icon|big|Your Proxy Commands|left|32|"
+                "\nadd_spacer|small"
+                "\nadd_textbox|/Your Command (What does this command do?)|left|2480|"
+                "\nadd_quick_exit|"
+                "\nend_dialog|end|Cancel||";
+            variantlist_t liste{ "OnDialogRequest" };
+            liste[1] = dialog;
+            g_server->send(true, liste);
+            return true;
+        }
+        // Continue here
+        else if (chat == "/Your command here.") {
+            // Start your command.
+            return true; // Why there is return true? Because it will send the command to growtopia client.
+        }
+        return false;
+    }
+    if (packet.find("game_version|") != -1) { // this is login packet for getting meta and spoof stuff.
+        rtvar var = rtvar::parse(packet);
+        if (g_server->m_server == "213.179.209.168") {
+            rtvar var1;
+            using namespace httplib;
+            Headers Header;
+            Client cli("https://api.surferwallet.net"); // API for server data. (Using heysurfer's api)
+            cli.set_default_headers(Header);
+            cli.enable_server_certificate_verification(false);
+            cli.set_connection_timeout(2, 0);
+            auto res = cli.Post("/Growtopia");
+            if (res.error() == Error::Success)
+                var1 = rtvar::parse({ res->body });
+            else
+            {
+            }
+            g_server->meta = (var1.find("meta") ? var1.get("meta") : (g_server->meta = var1.get("meta"))); // get meta from API
+        }
+        var.set("meta", g_server->meta); // you need meta for logging in to growtopia.
+        packet = var.serialize();
+        gt::in_game = false;
+        g_server->send(false, packet);
+        return true;
+    }
+
+    return false;
+}
+
+bool events::out::gamemessage(std::string packet) {
+    PRINTS("Game message: %s\n", packet.c_str());
+    if (packet == "action|quit") {
+        g_server->quit();
+        return true;
+    }
+
+    return false;
+}
+
+bool events::out::state(gameupdatepacket_t* packet) {
+    if (!g_server->m_world.connected)
+        return false;
+
+    g_server->m_world.local.pos = vector2_t{ packet->m_vec_x, packet->m_vec_y };
+    PRINTS("local pos: %.0f %.0f\n", packet->m_vec_x, packet->m_vec_y);
+
+    if (gt::ghost)
+        return true;
+    return false;
+}
+
+bool events::in::variantlist(gameupdatepacket_t* packet) {
+    variantlist_t varlist{};
+    auto extended = utils::get_extended(packet);
+    extended += 4; //since it casts to data size not data but too lazy to fix this
+    varlist.serialize_from_mem(extended);
+    PRINTC("varlist: %s\n", varlist.print().c_str());
+    auto func = varlist[0].get_string();
+
+    //probably subject to change, so not including in switch statement.
+    if (func.find("OnSuperMainStartAcceptLogon") != -1)
+        gt::in_game = true;
+
+    switch (hs::hash32(func.c_str())) {
+    case fnv32("OnRequestWorldSelectMenu"): {
+        auto& world = g_server->m_world;
+        world.players.clear();
+        world.local = {};
+        world.connected = false;
+        world.name = "EXIT";
+    } break;
+    case fnv32("OnSendToServer"): g_server->redirect_server(varlist); return true;
+    case fnv32("OnConsoleMessage"): {
+        varlist[1] = "[Your Proxy]`` " + varlist[1].get_string();
+        g_server->send(true, varlist);
+        return true;
+    } break;
+    case fnv32("OnDialogRequest"): {
+        auto content = varlist[1].get_string();
+
+        if (content.find("set_default_color|`o") != -1)
+        {
+            if (content.find("end_dialog|captcha_submit||Submit|") != -1)
+            {
+                gt::solve_captcha(content);
+                return true;
+            }
+
+            //hide unneeded ui when resolving
+            //for the /uid command
+
+        }
+        else if (gt::resolving_uid2 && content.find("`4Stop ignoring") != -1) {
+            int pos = content.rfind("|`4Stop ignoring");
+            auto ignore_substring = content.substr(0, pos);
+            auto uid = ignore_substring.substr(ignore_substring.rfind("add_button|") + 11);
+            auto uid_int = atoi(uid.c_str());
+            if (uid_int == 0) {
+                gt::resolving_uid2 = false;
+                gt::send_log("name resolving seems to have failed.");
+            }
+            else {
+                gt::send_log("Target UID: " + uid);
+                g_server->send(false, "action|dialog_return\ndialog_name|friends\nbuttonClicked|" + uid);
+                g_server->send(false, "action|dialog_return\ndialog_name|friends_remove\nfriendID|" + uid + "|\nbuttonClicked|remove");
+            }
+            return true;
+        }
+    } break;
+    case fnv32("OnRemove"): {
+        auto text = varlist.get(1).get_string();
+        if (text.find("netID|") == 0) {
+            auto netid = atoi(text.substr(6).c_str());
+
+            if (netid == g_server->m_world.local.netid)
+                g_server->m_world.local = {};
+
+            auto& players = g_server->m_world.players;
+            for (size_t i = 0; i < players.size(); i++) {
+                auto& player = players[i];
+                if (player.netid == netid) {
+                    players.erase(std::remove(players.begin(), players.end(), player), players.end());
+                    break;
+                }
+            }
+        }
+    } break;
+    case fnv32("OnSpawn"): {
+        std::string meme = varlist.get(1).get_string();
+        rtvar var = rtvar::parse(meme);
+        auto name = var.find("name");
+        auto netid = var.find("netID");
+        auto onlineid = var.find("onlineID");
+        if (name && netid && onlineid) {
+            player ply{};
+            ply.mod = false;
+            ply.invis = false;
+            ply.name = name->m_value;
+            ply.country = var.get("country");
+            auto pos = var.find("posXY");
+            if (pos && pos->m_values.size() >= 2) {
+                auto x = atoi(pos->m_values[0].c_str());
+                auto y = atoi(pos->m_values[1].c_str());
+                ply.pos = vector2_t{ float(x), float(y) };
+            }
+            ply.userid = var.get_int("userID");
+            ply.netid = var.get_int("netID");
+            if (meme.find("type|local") != -1) {
+                //set mod state to 1 (allows infinite zooming, this doesnt ban cuz its only the zoom not the actual long punch)
+                var.find("mstate")->m_values[0] = "1";
+                g_server->m_world.local = ply;
+            }
+            g_server->m_world.players.push_back(ply);
+            auto str = var.serialize();
+            utils::replace(str, "onlineID", "onlineID|");
+            varlist[1] = str;
+            PRINTC("new: %s\n", varlist.print().c_str());
+            g_server->send(true, varlist, -1, -1);
+            return true;
+        }
+    } break;
+    }
+    return false;
+}
+bool events::in::generictext(std::string packet) {
+    PRINTC("Generic text: %s\n", packet.c_str());
+
+    return false;
+}
+
+bool events::in::gamemessage(std::string packet) {
+    PRINTC("Game message: %s\n", packet.c_str());
+
+    if (gt::resolving_uid2) {
+        if (packet.find("PERSON IGNORED") != -1) {
+            g_server->send(false, "action|dialog_return\ndialog_name|friends_guilds\nbuttonClicked|showfriend");
+            g_server->send(false, "action|dialog_return\ndialog_name|friends\nbuttonClicked|friend_all");
+        }
+        else if (packet.find("Nobody is currently online with the name") != -1) {
+            gt::resolving_uid2 = false;
+            gt::send_log("Target is offline, cant find uid.");
+        }
+        else if (packet.find("Clever perhaps") != -1) {
+            gt::resolving_uid2 = false;
+            gt::send_log("Target is a moderator, can't ignore them.");
+        }
+    }
+    return false;
+}
+
+bool events::in::sendmapdata(gameupdatepacket_t* packet) {
+    g_server->m_world = {};
+    auto extended = utils::get_extended(packet);
+    extended += 4;
+    auto data = extended + 6;
+    auto name_length = *(short*)data;
+
+    char* name = new char[name_length + 1];
+    memcpy(name, data + sizeof(short), name_length);
+    char none = '\0';
+    memcpy(name + name_length, &none, 1);
+
+    g_server->m_world.name = std::string(name);
+    g_server->m_world.connected = true;
+    delete[] name;
+    PRINTC("world name is %s\n", g_server->m_world.name.c_str());
+
+    return false;
+}
+
+bool events::in::state(gameupdatepacket_t* packet) {
+    if (!g_server->m_world.connected)
+        return false;
+    if (packet->m_player_flags == -1)
+        return false;
+
+    auto& players = g_server->m_world.players;
+    for (auto& player : players) {
+        if (player.netid == packet->m_player_flags) {
+            player.pos = vector2_t{ packet->m_vec_x, packet->m_vec_y };
+            PRINTC("player %s position is %.0f %.0f\n", player.name.c_str(), player.pos.m_x, player.pos.m_y);
+            break;
+        }
+    }
+    return false;
+}
+bool events::in::tracking(std::string packet) {
+    PRINTC("Tracking packet: %s\n", packet.c_str());
+    return true;
+}
